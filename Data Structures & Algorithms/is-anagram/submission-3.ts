c2d1fa678b68d96af0c9ed83dbddeb91c05d9359
Robbe Verhelst <36887643+robbeverhelst@users.noreply.github.com>
2026-07-25 10:59:34 +0200

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) return false;
+
+        const sCount = new Map<string, number>();
+        const tCount = new Map<string, number>();
+
+        for (const char of s) {
+            sCount.set(char, (sCount.get(char) ?? 0) + 1);
+        }
+        for (const char of t) {
+            tCount.set(char, (tCount.get(char) ?? 0) + 1);
+        }
+
+        for (const [char, amount] of sCount) {
+            if (amount !== tCount.get(char)) return false;
+        }
+
+        return true;
+    }
+}
